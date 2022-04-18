9303db4887ae8073988650b7f6883fc78ca906df
elia Candela <eliaeco99@gmail.com>
2022-04-18 19:48:58 +0200

    Codigo final

--- src/get_pitch/pitch_analyzer.cpp
+++ src/get_pitch/pitch_analyzer.cpp
@@ -12,8 +12,7 @@ namespace upc {
 
     for (unsigned int l = 0; l < r.size(); ++l) {
   		/// \TODO Compute the autocorrelation r[l]
-      /// \FET -> autocorrelation calculated
-
+      /// \DONE Autocorrelation calculated
       r[l]= 0.0f;
       for(unsigned int n = l; n<x.size();n++){
         r[l] += x[n]*x[n-l];
@@ -36,13 +35,14 @@ namespace upc {
     switch (win_type) {
     case HAMMING:{
       /// \TODO Implement the Hamming window
-      /// \FET -> Hamming window done
+      /// \DONE Hamming window done
       float a0 = 0.53836F;
       float a1 = 0.46164F;
       for (unsigned int n = 0; n < frameLen; n++){
         window[n] = a0-a1*cos((2*M_PI*n)/(frameLen-1));
       }
     }
+
     break;
 
     case RECT:
@@ -51,12 +51,12 @@ namespace upc {
     }
   }
 
-  void PitchAnalyzer::set_f0_range(float min_F0, float max_F0) {
-    npitch_min = (unsigned int) samplingFreq/max_F0;
-    if (npitch_min < 2)
-      npitch_min = 2;  //samplingFreq/2
+   void PitchAnalyzer::set_f0_range(float min_F0, float max_F0) {
+     npitch_min = (unsigned int) samplingFreq/max_F0;
+     if (npitch_min < 2)
+       npitch_min = 2;  // samplingFreq/2
 
-    npitch_max = 1 + (unsigned int) samplingFreq/min_F0;
+     npitch_max = 1 + (unsigned int) samplingFreq/min_F0;
 
     //frameLen should include at least 2*T0
     if (npitch_max > frameLen/2)
@@ -67,14 +67,9 @@ namespace upc {
     /// \TODO Implement a rule to decide whether the sound is voiced or not.
     /// * You can use the standard features (pot, r1norm, rmaxnorm),
     ///   or compute and use other ones.
-    /// \FET -> detector de senyal unvoiced amb un nou parametre: umaxnorm
-
-    /*bool unvoiced = true;
-    if(rmaxnorm > umaxnorm){ //entonces es SONORO
-      unvoiced = false;
-    }
-    return unvoiced;*/
-    if ((pot < -40.0F || r1norm < 0.89F) && rmaxnorm < 0.5F)
+    /// \DONE Added new features as r1norm and rmaxnorm in order to calculate the unvoiced and voiced frames
+ 
+    if ((pot < -upot || r1norm < u1norm) && rmaxnorm < umaxnorm) 
       return true;
     else
       return false;
@@ -103,7 +98,7 @@ namespace upc {
 	///    - The lag corresponding to the maximum value of the pitch.
     ///	   .
 	/// In either case, the lag should not exceed that of the minimum value of the pitch.
-    /// \FET -> Maxim localitzat
+    /// \DONE Located maximum value of the autocorrelation
     for (iR = iRMax = r.begin() + npitch_min; iR < r.begin() + npitch_max; iR++) {
       if (*iR > *iRMax) {
         iRMax = iR;
@@ -115,11 +110,11 @@ namespace upc {
     float pot = 10 * log10(r[0]);
 
     
-
+    
     //You can print these (and other) features, look at them using wavesurfer
     //Based on that, implement a rule for unvoiced
     //change to #if 1 and compile
-#if 1
+#if 0
     if (r[0] > 0.0F)
       cout << pot << '\t' << r[1]/r[0] << '\t' << r[lag]/r[0] << endl;
 #endif
